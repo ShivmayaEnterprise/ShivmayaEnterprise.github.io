a2b2ee0920567412237cade89d23f0edc90844d5
ShivmayaEnterprise <surekapriyam07@gmail.com>
2026-03-29 20:39:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,40 @@ console.log("Website Loaded");
 const toggle = document.getElementById("darkToggle");
 
 toggle.addEventListener("click", () => {
+
 document.body.classList.toggle("dark");
 
-// optional icon change
 if(document.body.classList.contains("dark")){
 toggle.innerText = "☀️";
 } else {
 toggle.innerText = "🌙";
 }
+
 });
+
+/* SCROLL REVEAL */
+
+function reveal() {
+
+let reveals = document.querySelectorAll(".reveal");
+
+for (let i = 0; i < reveals.length; i++) {
+
+let windowHeight = window.innerHeight;
+
+let elementTop =
+reveals[i].getBoundingClientRect().top;
+
+let elementVisible = 100;
+
+if (elementTop < windowHeight - elementVisible) {
+
+reveals[i].classList.add("active");
+
+}
+
+}
+
+}
+
+window.addEventListener("scroll", reveal);
