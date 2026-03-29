3ffb22c90ca8d122c51b3cbc4cc74f64c9ce5db7
ShivmayaEnterprise <surekapriyam07@gmail.com>
2026-03-29 20:32:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 console.log("Website Loaded");
 const toggle = document.getElementById("darkToggle");
 
-toggle.onclick = () => {
-
+toggle.addEventListener("click", () => {
 document.body.classList.toggle("dark");
 
-};
+// optional icon change
+if(document.body.classList.contains("dark")){
+toggle.innerText = "☀️";
+} else {
+toggle.innerText = "🌙";
+}
+});
