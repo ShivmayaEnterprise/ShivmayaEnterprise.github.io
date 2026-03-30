7d76ed553b14a07285c26c4b29b79e96e0e46686
ShivmayaEnterprise <surekapriyam07@gmail.com>
2026-03-30 23:04:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,33 +23,6 @@ document.body.classList.add("dark");
 
 }
 
-/* SLIDER */
-
-let slides =
-document.querySelectorAll(".slide");
-
-let index = 0;
-
-function showSlide(){
-
-slides.forEach(s =>
-s.classList.remove("active")
-);
-
-index++;
-
-if(index >= slides.length){
-
-index = 0;
-
-}
-
-slides[index].classList.add("active");
-
-}
-
-setInterval(showSlide,4000);
-
 /* SCROLL REVEAL */
 
 function reveal(){
